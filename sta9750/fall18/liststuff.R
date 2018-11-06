5601a17b436185aef8bb212c961a4903cd59f6d1
Víctor Peña <VicPena@users.noreply.github.com>
2018-11-06 10:50:00 -0500

    Add files via upload

---
+++ sta9750/fall18/liststuff.R
@@ -0,0 +1,49 @@
+## Exercise 3, HW1 in R 
+# read-in school data
+school = read.csv("C:/Users/vpena/Downloads/school.csv")
+school$difference = school$after-school$before
+str(school)
+summary(school$difference)
+sd(school$difference)
+hist(school$difference)
+
+# 95% CI for the mean
+t.test(school$difference)
+?t.test
+
+str(school)
+
+## Lists
+
+v = 1:6
+m = matrix(c(1,0,0,1),byrow=T,nrow=2)
+# this creates a list whose entries are a vector and a matrix
+l = list(v,m)
+# you can index them like this
+l[[2]][2,1]
+l[[1]][4]
+
+# you can add a new element to the list indexing by a new element
+v2 = 3:4
+l[[3]] = v2
+
+
+## How to create and subset a dataframe
+
+a = 1:3
+b = c("a","b","c")
+# data.frames are "matrices" with columns that can
+# have mixed types (numeric, factor, date, etc.)
+df = data.frame(a,b)
+# we can access the columns by their name
+df$a
+df$b
+# or by their column index
+df[,1]
+df[,2]
+# can look at rows like this
+df[1,]
+df[2,]
+# and you can subset using logical conditions, like this:
+df[df$a>1,]
+
---
+++ sta9750/fall18/midterms.R
@@ -0,0 +1,56 @@
+# read in data
+midterms = read.csv("C:/Users/vpena/Downloads/midterms.csv")
+# structure of the dataset 
+str(midterms)
+
+# can create a variable whose entries are 'blue' if
+# the observation is a democrat, and red otherwise
+coldot = ifelse(midterms$party=='D','blue','red')
+coldot
+# some plots, pch = 16 for solid dots
+plot(x=midterms$year, y=midterms$approval, col=coldot, pch=16)
+plot(x=midterms$approval, y=midterms$change, col=coldot, pch=16)
+# fit linear model object
+mod = lm(change~approval, data=midterms)
+# overlay the regression line on the plot
+abline(mod)
+# summary of the regression
+summary(mod)
+par(mfrow=c(2,2)) # 2 by 2 matrix of plots 
+plot(mod) # diagnostic plots 
+
+# model doesn't fit very well...
+# let's try some transformations
+midterms$decperc = -midterms$change/435
+eps = 0.001
+k = -min(midterms$decperc)+eps
+midterms$decperc = midterms$decperc+k
+modlog = lm(log(decperc)~approval, data=midterms)
+modsqrt = lm(sqrt(decperc)~approval, data=midterms)
+
+par(mfrow=c(2,2)) # 2 by 2 matrix of plots 
+plot(modlog)
+
+par(mfrow=c(2,2)) # 2 by 2 matrix of plots 
+plot(modsqrt) 
+
+# modsqrt seems best
+
+# create a data.frame with entries where we want to predict
+appr = data.frame(approval=seq(0,100,0.01)) # seq in jumps of 0.01 from 0 to 100
+# predict with the modsqrt
+preds = predict(modsqrt, newdata=appr, interval='prediction')
+head(preds)
+# have to convert back to the original scale
+newpreds = preds^2-k
+newseats = newpreds*(-435)
+
+# can plot the non-linear relationship (in the original-scale)
+# and how the intervals look like
+plot(x=midterms$approval, y=midterms$change, col=coldot, pch=16,ylim=c(-100,10))
+points(x=appr$approval,y=newseats[,1], type='l')
+points(x=appr$approval,y=newseats[,2], type='l')
+points(x=appr$approval,y=newseats[,3], type='l')
+abline(v=41.9) # current Trump's approval rating
+# the prediction is quite wide...
+
