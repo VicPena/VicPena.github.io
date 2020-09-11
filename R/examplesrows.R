0ee6c796f5160cbde1de63c627cf75d0b1bf3326
Víctor Peña <VicPena@users.noreply.github.com>
2020-09-11 14:43:45 -0400

    Add files via upload

---
+++ R/examplesrows.R
@@ -0,0 +1,15 @@
+## Exercises on subsetting rows
+library(tidyverse)
+hsb2 = read.csv("http://vicpena.github.io/sta9750/spring19/hsb2.csv")
+str(hsb2)
+
+# What is the average `math` score in the dataset?
+
+# What is the average `math` score for those who scored 50 or greater in `read`? 
+
+# What is the average difference in `math` scores between individuals
+# whose race is `white` and those whose race is not `white`?
+
+# What is the percentage of individuals in the sample whose `race` is `white`?
+
+# What percentage of high `ses` students went to `public` schools?
