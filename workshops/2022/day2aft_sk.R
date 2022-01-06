204050e7178cf1b18fa58cbd614ae8a4d0768c26
Víctor Peña <VicPena@users.noreply.github.com>
2022-01-06 10:05:00 +0100

    Add files via upload

--- workshops/2022/day2aft_sk.R
+++ workshops/2022/day2aft_sk.R
@@ -2,9 +2,9 @@
 # Overview #
 ############
 # Plots!
-# - Univariate plots
-# - Bivariate plots
-# - More than two variables
+# - Univariate plots (one variable)
+# - Bivariate plots (two variables)
+# - More than two variables 
 # - Exercises
 # - Improving plots 
 # - Interactive plots with plotly
@@ -19,8 +19,13 @@
 
 
 library(tidyverse)
+
+
 install.packages("openintro")
 library(openintro)
+
+# ggplot for plotting
+
 
 data(hsb2)
 View(hsb2)
@@ -35,9 +40,63 @@ View(hsb2)
 # Categorical data: barplots
 
 # Plot of math
+# histogram
+ggplot(hsb2) +
+  aes(x = math) +
+    geom_histogram(bins = 30)
+
+# boxplot
+ggplot(hsb2) +
+  aes(x = math) +
+  geom_boxplot()
+
 
 # Plot of race
 
+# 1st step: create a table with counts
+# for the variable that I want to display
+tab = hsb2 %>% count(race) %>% mutate(perc = 100*n/sum(n))
+tab
+
+# ggplot(<dataset>) +
+# aes(x =, y =, color =, fill = , shape =  , linetype, ...) +
+# geom_point(), geom_col(), geom_line(), geom_smooth()... 
+# add facets, transformations, change themes, etc.
+
+
+# 2nd: ggplot on the table to create
+# the barplot
+ggplot(tab) +
+  aes(x = race, y = n) +
+    geom_col()
+
+ggplot(tab) +
+  aes(y = race, x = n) +
+  geom_col()
+
+# geom_col: stands for geom_column
+tab
+# create a plot with the percentages
+ggplot(tab) +
+  aes(y = race, x = perc) +
+    geom_col() +
+      xlab("percentages of ethnicities") +
+       ylab("ethnicity") +
+        xlim(0, 100) +
+          ggtitle("Percentages of ethnicities in hsb2 dataset")
+
+
+ggplot(tab) +
+  aes(x = race, y = perc) +
+  geom_col() +
+  ylab("percentages of ethnicities") +
+  xlab("ethnicity") +
+  ylim(0, 100) +
+  ggtitle("Percentages of ethnicities in hsb2 dataset")
+# xlab: x-label
+# ylab: y-label
+# xlim: limits of the x-axis when the x-axis is numeric
+# ylim: limits of the y-axis whenever it is numeric
 
 
 ###################
@@ -46,34 +105,156 @@ View(hsb2)
 
 # two numerical variables: scatterplot
 # one numerical one categorical: boxplots, histograms
-# two categorical: barplots
+# two categorical: barplots stacked or side-by-side
+
 
 # Plot of math against read
+# both numerical variables
+# scatter plot
+
+# ggplot(<dataset>) +
+# aes(x =, y =, color =, fill = , shape =  , linetype, ...) +
+# geom_point(), geom_col(), geom_line(), geom_smooth()... 
+# add facets, transformations, change themes, etc.
 
 # add in trend
+ggplot(hsb2) +
+  aes(x = read, y = math, color = ses, size = science) +
+    geom_point() +
+     geom_smooth(se = FALSE) +
+      scale_color_manual(values = c("blue","coral2","mediumpurple1")) +
+        facet_wrap(ses ~ gender, nrow = 3)
 
-# force trend to be linear
+
+ggplot(hsb2) +
+  aes(x = read, y = math, color = gender) +
+    geom_point() +
+      geom_smooth(method = "lm", se = FALSE)
 
 
-# 
+ggplot(hsb2) +
+  aes(x = read, y = math, color = gender) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~ gender)
+
+
+
+
+# we can include variables by
+# - using color-coding with color
+# - sizes of points with size
+# - facetting: up to two variables
+# if you do facetting with two variables:
+#  first: on the rows
+#  second variable: columns
+
+
+
+
+# an example of 
+# a plot displaying
+# - math, read, ses, gender
+
+# by default this is a non-linear trend
+# non-linear trend with 95% pointwise confidence interval 
+# (shaded region)
+
+ggplot(hsb2) +
+  aes(x = read, y = math) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) 
+# method = "linear model"
+# se = FALSE gets rid of the shaded region around the line
+
+
+# xlab, ylab, xlim, ylim, and ggtitle
+# all work in all types of ggplots
+
+# Plot of math (numerical) against ses (categorical)
+
+# ggplot(<dataset>) +
+# aes(x =, y =, color =, fill = , shape =  , linetype, ...) +
+# geom_point(), geom_col(), geom_line(), geom_smooth()... 
+# add facets, transformations, change themes, etc.
+
+ggplot(hsb2) +
+  aes(y = ses, x = math) +
+    geom_boxplot()
+
+# facet_wrap (graphical version of group_by)
+ggplot(hsb2) +
+  aes(x = math) +
+    geom_histogram() +
+      facet_wrap(~ ses)
+
+ggplot(hsb2) +
+  aes(x = math) +
+  geom_histogram() +
+  facet_wrap(~ race)
+# tilde ~ 
+
+ggplot(hsb2) +
+  aes(x = math) +
+    geom_boxplot() +
+      facet_wrap( ~ ses, nrow = 3)
+
+
 
 
 # Plot of ses against race
 
+# 1st step a table
+# that has counts of all combinations of ses and race
+tab2 = hsb2 %>% count(ses, race)
+
+# 2nd create the plot 
+# with ggplot
+ggplot(tab2) +
+  aes(x = ses, y = n, fill = race) +
+    geom_col()
+
+ggplot(tab2) +
+  aes(x = race, y = n, fill = ses) +
+  geom_col()
+
+
+# geom_col: creates barplots
 
 # position = "dodge"
+# install.packages("ggthemes")
+library(ggthemes)
+library(tidyverse)
+barplot = ggplot(tab2) +
+  aes(x = ses, y = n, fill = race) +
+   geom_col(position = "dodge") +
+    scale_fill_manual(values = c("lightslateblue",
+                                 "olivedrab3",
+                                 "plum2",
+                                 "firebrick2")) +
+     theme_excel_new()
 
+ggplotly(barplot)
+# fill: bars
+# color: coloring points
+
+# changing colors:
+# scale_fill_manual if used fill for defining the colors
+# scale_color_manual if used color for defining the colors
+
+
+# These are the ones that I like best
 # position = "fill"
+ggplot(tab2) +
+  aes(x = ses, y = n, fill = race) +
+  geom_col(position = "fill")
 
-# add in percentages 
+bar2 = ggplot(tab2) +
+  aes(x = race, y = n, fill = ses) +
+  geom_col(position = "fill")
 
-
-
-# Plot of math (numerical) against ses (categorical)
-
-
-# facet_wrap (graphical group_by)
-
+ggplotly(bar2)
+tab2
 
 
 
@@ -89,12 +270,36 @@ View(hsb2)
 
 
 # math scores vs reading scores by gender
+ggplot(hsb2) +
+  aes(x = read, y = math, color = gender) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~ gender)
 
 
 # math scores vs reading scores by gender and ses
+ggplot(hsb2) +
+  aes(x = read, y = math, color = gender) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(ses ~ gender, nrow = 3)
+
+ggplot(hsb2) +
+  aes(x = read, y = math, color = science) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~ ses, nrow = 3)
 
 
-# math scores vs reading vs write by gender and ses
+
+ggplot(hsb2)+
+  aes(x=read, y=math,color=ses, size=science)+
+  geom_point()+
+  geom_smooth(se=FALSE)+
+  scale_color_manual(values=c('blue','coral2','mediumpurple1'))+
+                    facet_wrap(ses~gender, nrow=3)
+
+
 
 
 
@@ -127,48 +332,37 @@ data(gapminder)
 # relationship of gdpcapita versus life expectancy by continent
 
 
-###################
-# improving plots #
-###################
-library(tidyverse)
-
-
-# change theme
-# Default themes: theme_minimal, theme_bw theme_light, etc.
-
-
-# library(ggthemes): https://github.com/jrnold/ggthemes
-install.packages("ggthemes")
-library(ggthemes)
-# some themes in ggthemes: theme_tufte, theme_wsj, theme_economist, theme_fivethirtyeight
-
-
-
-# change colors
-# List of colors: http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
-
-data(hsb2)
-# math vs write by ses
-# scale_color_manual(values = )
-# scale_color_palette(palette = " ")
-# https://ggplot2.tidyverse.org/reference/scale_brewer.html
-
-# race and ses
-# scale_fill_manual(values = )
-
 
 #################################
 # Interactive plots with plotly #
 #################################
-
 install.packages("plotly")
 library(plotly)
 
-# repeat plot life exp (numeric) v gdp per capita (numeric) 
+# plot life exp (numeric) v gdp per capita (numeric) 
 # using data from 2007 only
 # add in countries as labels
+library(gapminder)
+data(gapminder)
+gapminder
+
+# it is useful to create a dataset that has 
+# dta with 2007 only 
+# that can be accomplished with a filter! 
+# which we saw
+gap2007 = gapminder %>% filter(year == 2007)
+# plot life exp (numeric) v gdp per capita (numeric) 
 
 
-# save ggplot 
+# each point is a different country
+# how can I identify the countries?
 
-# run ggplotly on saved plot
+# add in label = country in aes, and save ggplot 
+interact = ggplot(gap2007) +
+  aes(x = lifeExp, y = gdpPercap, size = pop, label = country, color = continent) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
+# run ggplotly function on saved plot
+ggplotly(interact)
+
--- workshops/2022/day2morn_sk.R
+++ workshops/2022/day2morn_sk.R
@@ -3,6 +3,19 @@
 ##########
 # email: victor.pena@baruch.cuny.edu
 
+# creating variables of different types:
+# - numeric, character, factor, logical
+# - vectors: allowed to keep more than one observation in a single variable
+# - data.frames: data structures that have rows and columns
+
+data(iris)
+class(iris)
+# data.frame
+
+# saving results into csv files: write.csv
+
+# create summary statistics
+# by groups and without groups
 
 ############
 # Overview #
@@ -17,7 +30,7 @@
 # Recoding factors #
 ####################
 
-library(readr)
+library(tidyverse)
 interfaith <- read_table2("http://users.stat.ufl.edu/~winner/data/interfaith.dat", 
                           col_names = FALSE)
 
@@ -54,17 +67,72 @@ interfaith
 # Exercises #
 #############
 
+# install.packages("openintro")
+library(openintro)
 data("absenteeism")
 ?absenteeism
 
-# find average days of absence and std dev by
-# ethnicity status
+absenteeism %>% view
 
-# do the same but for all combinations of ethnicity
-# status and sex
+# mean sd days aboriginal students
+# mean sd days not aboriginal students
 
+# group_by & summarize
+# library(tidyverse)
+# install.packages("tidyverse")
+library(tidyverse)
+
+# find average days of absence and std dev separately by
+# each ethnicity status
+absenteeism %>% group_by(eth) %>% summarize(mean_days = mean(days),
+                                            med_days = median(days),
+                                            sd_days = sd(days))
+
+# do the same but for all combinations of ethnicity sex
+
+# eth sex
+# A   M
+# A   F
+# N   M
+# N   F
+
+# write code that finds me the means and std deviations
+# of days of absence defined by all the combinations of 
+# ethnicity and sex
+absenteeism
+# I want separate summary statistics for 
+# the values of ethnicity
+# ethnicity:
+# - A (aboriginal) and N (not aboriginal)
+
+absenteeism %>% group_by(eth, sex) %>% summarize(mean_days = mean(days),
+                                                 sd_days = sd(days))
+
+absenteeism
 # find percentages of lrn (learner status)
 # by ethnicity
+
+# learner status
+# ethnicity
+
+# Among Aboriginals: % of slow learners and % of average learners
+# Among Non-aboriginal students: % of slow learners and average learners
+
+absenteeism %>% count(eth, lrn) %>% mutate(perc = 100*n/sum(n))
+# but these percentages are not separate by aboriginal and non-aboriginal
+# out of the total population
+# out of the total population, 27.4% of the students are aboriginal
+# and average learners
+
+
+
+# percentages grouped by the ethnicity status
+# add the fact that we want to separate our analyses
+# according to ethnicity status
+absenteeism %>% group_by(eth) %>% count(eth, lrn) %>% mutate(perc = 100*n/sum(n))
+# separate percentages for aboriginal and non-aboriginal
+# 58% of aboriginal are average learners, 42% are slow
+# 55.8% of non-aboriginals are average learners, 44.2% slow 
 
 
 ################################
@@ -72,9 +140,19 @@ data("absenteeism")
 ################################
 
 
+# subsets of rows using slice
+
+# first 5 rows of iris
+iris %>% slice(1:5)
+
+# filter is a function that lets us get subsets of the data
+# that satisfy logical conditions
+
 # in practice, we rarely do this. we usually want to
 # get subsets of data that satisfy some condition
 iris_greater5 = iris %>% filter(Sepal.Length > 5)
+
+iris_greater5
 
 ##
 ## Logical operators in R
@@ -87,8 +165,15 @@ iris_greater5 = iris %>% filter(Sepal.Length > 5)
 # <=  less than or equal to
 
 # observations where Species is not setosa
-iris %>% filter( Species != "setosa") # remember to put character values in quotation marks!
-iris %>% filter(Species == "setosa")
+iris_not_setosa = iris %>% filter(Species != "setosa")
+
+# observations where Species is only viriginica
+iris_virginica = iris %>% filter(Species == "virginica")
+iris_virginica
+
+# observations where Sepal.Width is less than or equal to 3
+iris_small = iris %>% filter(Sepal.Width <= 3)
+iris_small
 
 
 ## Combining logical conditions
@@ -100,18 +185,63 @@ iris %>% filter(Species == "setosa")
 # | or
 # ! not
 
-# setosas whose Sepal.Width is less than or equal to 4
-iris %>% filter(Species == "setosa" & Sepal.Width <= 4)
+# virginica such that petal length is greater than 2
+# a subset of the data that only includes 
+# flowers whose species is virginica AND 
+# their petal length is greater than 2
 
-# setosas or versicolor whose Sepal.length is greater than 5
-iris %>% filter( (Species == "setosa" | Species == "versicolor") & Sepal.Length > 5) 
+iris_sub = iris %>% filter(Species == "virginica" & Petal.Length > 2)
+iris_sub
 
-# we can create new variables using logical conditions with mutate
-iris %>% mutate( bigLength = Sepal.Length > 5)
-# if you want to convert TRUE to 1 and FALSE to 0
-iris %>% mutate( bigLength = 1*(Sepal.Length > 5))
-# can use this to our advantage with complicated logical conditions
-iris %>% mutate( cond =  1*((Species == "setosa" | Species == "versicolor") & Sepal.Length > 5))
+iris_sub2 = iris %>% filter(Species == "virginica" | Petal.Length > 2)
+# all flowers that are either virginica or have a petal length greater than
+# two or both conditions are satisfied
+iris_sub2
+
+# Exercise regarding filter
+# create a dataset that has only setosas whose petal.length is  greater than 4
+# or their petal width is greater than 2 
+
+iris %>%
+  filter((Species == "setosa" & Petal.Length > 4) | (Petal.Width > 2) )
+
+# To be in you can be either:
+# - Setosa with petal length greater than 4
+# - Any flower with petal width greater than 2 
+
+iris %>%
+  filter((Petal.Length > 4 | Petal.Width > 2) & (Species == "setosa"))
+
+# for & to satisfied both conditions have to be satisfied
+
+
+
+# To be in here both:
+# - Setosa
+# - and Have petal length greater than 4 or Petal width greater than 2
+
+
+
+
+
+# Give those that (either setosa and whose petal length is greater than 4)
+# or those flowers whose petal width is greater than 2
+
+
+iris %>%
+  filter(Species == "setosa" & (Petal.Length > 4 | Petal.Width > 2))
+
+
+iris %>%
+  filter((Petal.Length > 4 | Petal.Width > 2))
+
+# Whenever we write complex logical conditions 
+# make sure to write parentheses to understand the kind of 
+# restriction we are applying to our data
+
+# it's easy to make mistakes and end up with empty subsets of 
+# data when we code these things 
+
 
 
 
@@ -120,8 +250,13 @@ iris %>% mutate( cond =  1*((Species == "setosa" | Species == "versicolor") & Se
 #############
 
 # install.packages("gapminder"): command for installing the package
+# install.packages("gapminder")
+
+# information on countries over the years
+# lifeExp, gdpPercap by continent over the years
 library(gapminder)
 data(gapminder)
+view(gapminder)
 ?gapminder
 
 # group_by + summarize:
@@ -129,14 +264,87 @@ data(gapminder)
 # for different categories
 
 # average life expectancy by continent?
+# ignore the year; combine from all years
+gapminder %>%
+  group_by(continent) %>%
+  summarize(avg_lifeExp = mean(lifeExp)) 
+
+
+gapminder %>% 
+  group_by(continent, year) %>% 
+  summarize(avg_lifeExp = mean(lifeExp)) %>% 
+  view
 
 # average life expectancy by country along with its sd
+gapminder %>%
+  group_by(country) %>%
+    summarize(avg_LE = mean(lifeExp),
+              sd_LE = sd(lifeExp)) %>%
+                view
 
-# average life expectancy by continent in 1952?
+# average life expectancy by continent 
+# looking only at data in 1952?
+
+# run filter first so that you only work with data
+# from 1952 
+# and then group_by and summarize to get the answer
+
+gapminder %>% filter(year == 1952) %>% 
+                group_by(continent) %>%
+                  summarize(avg_LE = mean(lifeExp))
+
+
+gapminder %>% 
+  group_by(continent) %>%
+  filter(year == 1952) %>% 
+  summarize(avg_LE = mean(lifeExp))
+
+
+# when the variable is categorical we need quotation marks
+# when we deal with numbers we don't
 
 # average population by continent in 1997?
+gapminder %>% filter(year == 1997) %>% 
+  group_by(continent) %>%
+  summarize(avg_pop = mean(pop))
 
-# find average life expectancy by continent in 1952 and 2007?
+# table with average life expectancy by continent in years 1952 and 2007?
+# averages for 1952
+gapminder %>% 
+  group_by(continent) %>%
+  filter(year == 1952) %>% 
+  summarize(avg_LE = mean(lifeExp))
+# averages for 2007
+gapminder %>% 
+  group_by(continent) %>%
+  filter(year == 2007) %>% 
+  summarize(avg_LE = mean(lifeExp))
+
+# table with average life expectancy by continent in years 1952 and 2007?
+# averages for 1952
+
+# it makes sense to filter our data
+# so that we only have data from those years
+gapminder %>% 
+  filter(year == 1952 | year == 2007) %>%
+  group_by(continent, year) %>% 
+  summarize(avg_LE = mean(lifeExp))
+
+
+# I could do the following
+# I can find all the averages for all combinations of year
+# and continent and after that, filter out only the years that 
+# I care about
+gapminder %>%
+  group_by(continent, year) %>% 
+    summarize(avg_LE = mean(lifeExp)) %>%
+      filter(year == 1952 | year == 2007)
+
+# combining group_by, summarize, filter, and count
+# we can get summary statistics that are as complex and detailed as 
+# we want
+
+
 
 
 #############################
@@ -145,25 +353,55 @@ data(gapminder)
 
 # missing data are marked with NA: Not Available
 x = c(1:5, NA)
+# NA: not available
 x
 
 # applying functions to vectors with missing data is tricky
 # for example, let's take the mean of x
+mean(x)
+sd(x)
+# NA
+# if I apply any summary statistic to a vector containing 
+# one missing value then the answer is going to be NA
+
+mean(x, na.rm = TRUE)
+sd(x, na.rm = TRUE)
+# na.rm = NA remove
+# takes out the missing data and then finds average
+
+
 
 # by default, if there is an NA in our data and we try to compute
 # any function with it, the answer is going to be NA
 
 # arithmetic with NAs is NA
+5+NA
+0*NA
+# whenever we have a missing value and we do an operation with 
+# it the result is going to be missing
 
 
 # load in airquality
 data("airquality")
-?airquality
+view(airquality)
 summary(airquality)
+# the number of missing values appears in the 
+# summary output
+# for example, we got 37 missing values of Ozone
+
+airquality %>% summarize(avg_ozone = mean(Ozone))
+airquality %>% summarize(avg_ozone = mean(Ozone, na.rm = TRUE))
+# if I want to see actual averages with summarize
+# I need to add na.rm = TRUE
+
 
 
 # na.omit: keep complete cases only
 # usually not recommended unless you have 
 # a very good reason to drop the missing data
 air_clean = na.omit(airquality)
+
+summary(air_clean)
+# if you run na.omit you get out a dataset
+# that has NO missing values
 
