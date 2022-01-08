bb76512f72761584237ab49dd1940a4d2db5fa07
Víctor Peña <VicPena@users.noreply.github.com>
2022-01-08 10:44:03 +0100

    Add files via upload

--- workshops/2022/day3aft_sk.R
+++ workshops/2022/day3aft_sk.R
@@ -2,15 +2,193 @@
 #############################
 # Fitting regression models #
 #############################
+# read in http://vicpena.github.io/sta9750/spring19/nyc.csv
+nyc <- read_csv("http://vicpena.github.io/sta9750/spring19/nyc.csv")
+nyc = nyc %>% mutate(Location = East)
+# create subset with some variables
+# select function: lets me get subsets of 
+# variables in a dataset
+nyc_sub = nyc %>% select(Price, Service, Decor, Food, East)
 
-# fit a model where the outcome is `Price` 
+# load library(GGally)
+library(GGally)
+
+# ggpairs
+ggpairs(nyc_sub)
+
+# ggcorr (correlations)
+# it only makes sense for numerical variables
+# create a subset of the data that only includes
+# the numerical variables
+# with that, I will use ggcorr
+nyc_num = nyc %>% select(Price, Service, Decor, Food)
+ggcorr(nyc_num, label = TRUE)
+# heatmap: intensity of the correlations
+
+
+# Predict the Price of a meal given 
 # and the predictors are `Food`, `Decor`, `Service`, and `East`
+# lm: linear model (regression model)
+mod = lm(Price ~ Food + Decor + Service + Location, data = nyc)
+# outcome that I want to predict: Price
+# after the tilde (~)
+# I put in the predictors which are the variables that I
+# will use to predict the outcome
+
+
+
+# we can a regression table with the command
+# summary
+summary(mod)
+
+# residuals: observed values - predicted values by model
+
+# min: -13.7995
+# there is a restaurant whose predicted Price is 
+# $13.7995 more expensive than its actual observed Price
+
+# max: 16.8484
+# there is a restaurant whose predicted Price 
+# is $16.8484 cheaper than its actual observed Price
+
+
+# Estimates: Least-squares estimates
+# of the coefficients
+
+# Regression equation:
+
+# Price = -23.644163 + 1.634869*Food + 1.875549*Decor + 0.007626*Service - 1.613350*LocationWest
+
+# East side, regression equation:
+# Price = -23.644163 + 1.634869*Food + 1.875549*Decor + 0.007626*Service 
+# West side, regression equation:
+# Price = (-23.644163 - 1.613350) + 1.634869*Food + 1.875549*Decor + 0.007626*Service 
+
+# LocationWest = 1 if restaurant is on the West side 
+#                  and it's 0 on the East side
+
+# null: all coefficients are zero
+# alternative: there is at least a coefficient
+#              that is non-zero
+# p-value: 2.2*10^(-16)
+
+
+# predict: allows us to make predictions for any new restaurants
+
+
 
 # model diagnostics
+plot(mod)
+# fitted vs residuals: if the assumptions of the 
+# model are met you should see a cloud of points
+# with no discernible patterns
+# used to detect deviations from linearity
+
+# Linear model has assumption
+# Linearity: there is an underlying linear trend in the data
+# Independence: the errors do not depend upon each other
+# Normality: the errors are normal 
+# Equality of variance (homoskedasticity): the variance of the error does not depend on x
+
+# Linearity: residuals vs fitted; if assumption satisfied we see a cloud of points
+# with no discernible patters
+
+# Independence: we usually don't check unless we have a time variable
+
+# Normality: we check with the qq-plot; if the points are 
+# near the dashed line, then the assumption is met
+
+# Equality of variances: we usually check with residuals vs 
+# fitted; if the assumption is satisfied, we see that the spread
+# on the y-axis doesn't depend on where we are on the x-axis
+
+
+
+ggplot(nyc) +
+  aes(y = Price, x = Food) +
+      geom_point() +
+        geom_smooth(method = "lm")
+
+# residuals vs fitted values
+# observed - predicted
+# fitted: line itself
+
+
+
+# qq-plot: is used to assess the assumption of 
+# normality of residuals. if the assumption is met
+# then the points should be near the line
+# in this case, the plot is good; the points are 
+# near the line
+# if normality is not satisfied, you see an S shaped
+# plot
+
+# sqrt(abs(standardized residuals)) against fitted values
+# the interpretation is the same as the interpretation of the 
+# first one
+
+
+# hatvalues: used for identifying usual combiations of the predictors
+# if an observation has a large hatvalue it means that its 
+# predictors are unusual in some sense
+
+# two points that have unsual combinations of predictors
+
+# add a column to our dataset with the hat values
+nyc = nyc %>% mutate(hatvalues = hatvalues(mod))
+# and then filter and identify the restaurants
+nyc %>% filter(hatvalues > 0.20)
+# we can take a look at the values of their predictors
+# and understand why they are unusual
+
+# Food, Decor, Service, Location
+summary(nyc)
+# Veronica: Very small value of Decor (min in the sample)
+#           but its quality of Food is very good and the Service
+#           is also very bad (min in the sample)
+
+# Gennaro: Small values of Decor and Service  but good quality of Food 
+# predict
+
+# useful for making predictions without having to use the 
+# regression equation manually
+
+# Suppose that I want to predict the price of a meal
+# in the following two restaurants
+
+# restaurant 1: Food = 20, Service = 14, Decor = 23, Location = "West"
+# restaurant 2: Food = 14, Service = 24, Decor = 24, Location = "East"
+
+# 1st. define a data.frame which has the data for the new restaurants
+newrest = data.frame(Food = c(20, 14),
+                     Service = c(14, 24),
+                     Decor = c(23, 24),
+                     Location = c("West", "East"))
+
+newrest
+
+# 2nd. using predict
+predict(mod, newdata = newrest, interval = "prediction", level = 0.99)
+# prediction intervals for the prices
+# if you want 0.95 you can change that to 0.95 
+# (or don't write anything, because 0.95 is the default value)
+
+
 
 # effect plots with library(jtools)
-install.packages("jtools")
+# install.packages("jtools")
 library(jtools)
+effect_plot(mod, Food)
+# set the numerical variables
+# to the averages in the sample
+# and the categorical variables to their baseline
+# and visualize the effect of Food on the outcome
+
+effect_plot(mod, Location)
+# no interactions
+# this gives me predicted values for East and West
+# along with 95% confidence intervals
+
 
 
 #############################
@@ -18,24 +196,38 @@ library(jtools)
 #############################
 
 # backward selection
+# start out with all the variables
+# under consideration 
+# and drop variables one by one until 
+# my model gets worse according to some criterion
 
-# forward selection
-# fwd = step(mod_null,
-#            scope = formula(mod_full),
-#            direction = "forward")
+# a criterion to score the model 
+# by default R uses AIC (Akaike Information Criterion)
+# AIC: is a criterion that was developed to select
+# the best model for prediction (under certain
+# theoretical conditions)
+mod_all_variables = lm(Price ~ Food + Service + Decor + Location, data = nyc)
+bwd_AIC = step(mod_all_variables, direction = "backward")
+# The smaller the AIC, the better
+# Starting AIC: 526.64
+summary(bwd_AIC)
 
-# stepwise selection
 
 
-##############
-# Prediction #
-##############
 
-# Predict `Price` for restaurants:
-# Mario's: Food = 20, Decor = 20, Service = 19, East side
-# Luigi's: Food = 15, Decor = 23, Service = 23, West side
+mod_no_variables = lm(Price ~ 1, data = nyc)
+summary(mod_no_variables)
+fwd_AIC = step(mod_no_variables,
+               scope = formula(mod_all_variables),
+               direction = "forward")
 
-# predict(mod, newdata, interval = "prediction", level)
+# stepwise regression
+# where you start with nothing (like forward)
+# and then at each step you can either add or drop variables
+stepwise_AIC = step(mod_no_variables,
+               scope = formula(mod_all_variables),
+               direction = "both")
+summary(stepwise_AIC)
 
 
 ################
@@ -43,14 +235,32 @@ library(jtools)
 ################
 
 # simply write the product
+# Quality of Food and Location
+# The effect of quality of food on the Price
+# is dependent on whether the restaurant is on the East
+# or the West side
+mod_inter = lm(Price ~ Food + Food*Location + Location + Decor + Service,
+               data = nyc)
 
-# fit model that predicts Price 
-# given Food and interaction with East 
-
+summary(mod_inter)
+# a different slope for Food for restaurants on the West side
+# slope for Food on the East side: 1.67086
+# slope for Food on the West side: 1.67086-0.09881
 
 # use interact_plot in library(interactions)
 # to visualize interaction
+library(interactions)
+interact_plot(mod_inter, Food, Location)
+# setting all the other variables to their 
+# average in the sample
+# we can visualize the interaction effect
+mod_inter2 = lm(Price ~ Food + Location*Service + Location + Decor + Service,
+               data = nyc)
+interact_plot(mod_inter2, Service, Location)
+summary(mod_inter2)
 
-
-
+# mediation analysis can be done in R
+# product test
+# bootstrap test
+# 
 
--- workshops/2022/day3morn_sk.R
+++ workshops/2022/day3morn_sk.R
@@ -1,25 +1,46 @@
 ############
 # Overview #
 ############
+
+# - group_by and summarize
+# - Plots: afternoon 
+
+# - Plots: odds & ends
 # - Confidence intervals and hypothesis testing
-# - Exploratory data analysis
+# - Exploratory data analysis: 
+#     summary statistics and plots
 
 ###################### 
 # Plots: odds & ends #
 ######################
 
 library(tidyverse)
+# ggplot, group_by, summarize, mutate, and count
 library(openintro)
 
 data(hsb2)
+
+# 1st step: creating a table
+# that had the information that we want
+# to display using count
+
+# create a plot with ses and race
 tab2 = hsb2 %>% count(ses, race)
+tab2
+
+# ggplot(...) +
+# aes(x =, y = , fill = , ...) +
+# geom_col
+
+# geom_col: "geom column"
+# geom_histogram: histograms
+# ...
 
 # changing scale from proportions to percentages
 # in geom_col(position = "fill")
 ggplot(tab2) +
   aes(x = ses, y = n, fill = race) +
-  geom_col(position = "fill") +
-  scale_y_continuous() 
+  geom_col(position = "fill") 
 
 
 ggplot(tab2) +
@@ -31,8 +52,26 @@ ggplot(tab2) +
 # adding counts / percentages to bars
 library(openintro)
 data(immigration)
+immigration
+# response to a question: what we should we do
+#  with illegal immigrants that are caught?
+# political affiliation 
+
+immigration %>% count(response)
+immigration %>% count(political)
+
+# have a table that gives me the responses grouped by
+# the political affiliation
+
+# the response to the question for conservatives, 
+# liberals and moderates separately
 
 immi_counts = immigration %>% group_by(political) %>% count(response)
+immi_counts
+
+ggplot(immi_counts) +
+  aes(x = political, y = n, fill = response) +
+    geom_col()
 
 ggplot(immi_counts) +
   aes(x = political, y = n, label = n, fill = response) +
@@ -42,13 +81,18 @@ ggplot(immi_counts) +
 ggplot(immi_counts) +
   aes(x = political, y = n, label = n, fill = response) +
   geom_col(position = "dodge") +
-  geom_text(position = position_dodge(0.9))
+   geom_text(position = position_dodge(0.9))
 
+
+
+immi_counts = immi_counts %>% group_by(political) %>%
+  mutate(perc = paste(round(100*n/sum(n),1), "%", sep = ""))
 
 ggplot(immi_counts) +
-  aes(x = political, y = n, label = round(100*n/sum(n),2), fill = response) +
+  aes(x = political, y = n, label = perc, fill = response) +
   geom_col(position = "fill") +
-  geom_text(position = position_fill(0.5))
+  geom_text(position = position_fill(0.5)) + 
+  scale_y_continuous(labels = scales::percent) 
 
 
 # adding error bars to bar plots
@@ -67,50 +111,257 @@ library(tidyverse)
 
 # scientists recorded some measurements stored in x
 x = c(1,4,2,3,6,9,1,3,9,3)
+df = as.data.frame(x)
+df
 
 # visualize data
+# numerical variable: boxplot, histogram
+
+# 1st. convert x to data.frame
+# ggplot expects inputs of type data.frame
+df
+# ggplot(..) +
+#  aes(x , y ) +
+#     geom_boxplot()
+ggplot(df) +
+  aes(x = x) +
+    geom_boxplot()
+
+
+
 
 
 # provide a 99% confidence interval for the population mean
 # (interval based on normal distribution)
 
-# Test H0: population mean is equal to 0 
-#      H1: population mean is NOT equal to 0
+# assuming that x is a simple random sample from
+# some population and we want to find a confidence interval
+# for the population mean 
+
+# an interval based on the t.test 
+# the interval will be valid provided that the conditions 
+# under which the t.test is valid are satisfied
+
+# x is data.frame and it has a column named x
+# if I want to use this variable x into the t.test function
+# I have to reference variable x within the data.frame x
+
+# if you have a data.frame called df in R
+# and there is a variable called x in it
+# we can reference it with the command 
+# df$x
+
+# df: x
+# variable: x
+# x$x
+t.test(df$x, conf.level = 0.99)
+# 99 percent confidence interval:
+# 1.057163 7.142837
+
+# 95% confidence
+t.test(df$x, conf.level = 0.95)
+# 95 percent confidence interval:
+#   1.981931 6.218069
+
+# suppose that I want to find a 95% 
+# confidence interval for the read scores
+# this a sample of 200 students from a larger population of 
+# students... I want to get a confidence interval for the 
+# population mean of read scores
+t.test(hsb2$read, conf.level = 0.95)
+# 95 percent confidence interval:
+# 50.80035 53.65965
+# 95% confidence interval for the population mean
+# of reading scores in the population
+
+hsb2
+# scores in read, write, math, science, socst
+
+# 1st. I want to compute a variable that has 
+# the average scores in these subjects
+# (Hint: use the mutate function to create
+# the new variable)
+hsb2_male = hsb2 %>% 
+            mutate(avg = (read+write+math+science+socst)/5) %>%
+                  filter(gender == "male") %>%
+                    select(race, avg)
+# f, g, h to say x
+# f(g(h(x)))
+# apply h first, then g, then f 
 
 
 
-# Test H0: population mean is equal to 5
-#      H1: population mean is less than 5
 
 
-# Non-parametric alternative: wilcox.test
+hsb2
 
-# comparing two means
-#---------------------
+# mutate: creates new columns
 
-# A pharmaceutical is interested in knowing whether 
-# their new treatment is significantly different 
-# than the current gold standard. 
-# They collected a sample of 40 individuals:
-# 20 of them were assigned the new treatment, 
-# and 20 of them were assigned the current treatment. 
-# The outcome is on an ordinal scale that goes 
-# from 0 to 100, where 0 is "bad" and 100 is "great".
+# %>%: functions in library(tidyverse)
+# - filter: creates subsets of data satisfying logical conditions
+# - slice: creates subsets of data using row numbers
+# - select: creates subsets of columns
+# - group_by & summarize & count
+# - mutate
 
-# read in the data from https://vicpena.github.io/sta9750/fall18/pharma.csv
+hsb2
+# 2nd. I want a 95% confidence interval for that 
+# average
+t.test(hsb2$avg, conf.level = 0.95)
+# 95 percent confidence interval:
+# 51.24162 53.52038
 
-# find means and standard deviations by group
+# mean of hsb2$avg: 52.381 
 
-# visualize the data
+# null hypothesis: population mean of avg is 0
+# alternative hypothesis: population mean of avg is not 0
 
-# find 95% confidence interval for difference
-# (pop mean current treatment) - (pop mean new treatment)
+# t = 90.657, t-statistic
+# df = 199, degree of freedom
+# p-value < 2.2e-16
+# p-value that is less than 2.2*10^(-16) 
+# smaller than any reasonable significance level
 
-# Test H0: treatments are the same
-#      H1: treatments are different
+# conclusion: reject the null the hypothesis
 
 
-# Non-parametric alternative: wilcox.test
+# if we want to change the null hypothesis to say
+# null hypothesis: the population average of avg score 
+#                  is 50
+t.test(hsb2$avg, mu = 50)
+# H0: population mean = median = 50
+# H1: population mean = median is not equal to 50
+
+# t = 4.1209, df = 199, p-value = 5.536e-05
+# alternative hypothesis: true mean is not equal to 50
+# p-value = 5.536*10^(-5) small number
+# a number that is smaller than 0.01 so we would reject 
+# the null hypothesis that the population average of hsb2$avg
+# is equal to 50
+
+# a non-parametric alternative to
+# the t-test is the wilcox.test which is based 
+# on ranks of the data
+
+# the assumption of normality is suspect in the data
+# you can use wilcox.test which does not hinge on that assumption
+wilcox.test(hsb2$avg, mu = 50)
+# does not assume normality
+# null: median is equal to 50
+# alternative: median is not equal to 50
+
+# p-value = 0.0001467 
+# which is still smaller than 0.01
+
+# H0: median is equal to 50
+# H1: median is not equal to 50
+# rejecting the null hypothesis
+
+ggplot(hsb2) +
+  aes(x = avg) +
+    geom_histogram() +
+      geom_vline(xintercept = 50, color = "red")
+
+# CI & tests for 
+# - one numerical variable
+
+
+
+
+# comparing two numerical means #
+#-------------------------------#
+
+# Let's compare the math scores
+# for students that go to private school
+# against those that go to public school
+
+# null: population avg math score is the 
+# same for students that go to private or
+# public school
+
+# alt: pop averages are different
+
+# good idea to plot the data 
+# numerical: math
+# categorical: schtyp
+# histograms by groups
+# boxplots by group
+ggplot(hsb2) +
+  aes(x = schtyp, y = math) +
+    geom_boxplot()
+
+# t.test
+# we are comparing two groups
+t.test(hsb2$math ~ hsb2$schtyp)
+# df = 45.351: assuming that the variances
+# of the groups are not equal
+# p-value = 0.1545 > 0.01 significance level
+# so the conclusion would be do not reject the null 
+# (that the average scores do not depend on the schtyp)
+?t.test
+
+
+
+
+
+# 95% confidence interval for the difference in scores
+# public - private
+#  [-5.9908645, 0.9789598]
+# the interval contains 0 so the null hypothesis cannot be 
+# ruled out at the 0.05 significance level
+
+# if I want to change the level of the confidence interval
+# I can do so with conf.level
+t.test(hsb2$math ~ hsb2$schtyp, conf.level = 0.99)
+# 99 percent confidence interval:
+# [-7.159029, 2.147124]
+
+# if we don't specify anything the default confidence
+# level in t.test is 0.95
+
+
+t.test(hsb2$math ~ hsb2$schtyp, var.equal = FALSE)
+t.test(hsb2$math ~ hsb2$schtyp, var.equal = TRUE)
+# assume that the population variances of the groups
+# are the same
+
+# ANOVA with a factor that only has two levels
+# it's the same as doing the t-test with equal variances
+# F = t^2 
+
+# ANOVA:
+# - equality of variances
+
+
+# simulate from two normal distributions
+x = rnorm(100, mean = 0, sd = 1)
+y = rnorm(100, mean = 0, sd = 10)
+df = data.frame(outcome = c(x,y), group = rep(c("x", "y"), each = 100))
+df
+ggplot(df) +
+  aes(x = group, y = outcome) +
+    geom_boxplot()
+
+# ANOVA
+# - normality
+# - equality of variances
+# - independence of observations
+
+# some people test the assumptions visually
+# - histograms to see if "data look normal"
+# - qq-plot: if the data are roughly normally distributed
+#            the points (data) lie near a straight line
+# - equality of variance can be assessed with a plot like
+# the one I just did
+
+# some people run formal hypothesis tests
+# - normality: kolmogorov.smirnov, jarque.bera, shapiro.wilk, ...
+# - equality of variances: f-test, levene test, ...
+
+# some people do both
+mod = lm(hsb2$math ~ hsb2$schtyp)
+summary(mod)
+
 
 ## One proportion 
 
@@ -119,63 +370,151 @@ x = c(1,4,2,3,6,9,1,3,9,3)
 # In a 2012 poll by Harris Interactive, 
 # 59 out of 423 respondents said yes. 
 # Based on the data in this study, 
+
 # find a 99% confidence interval the proportion of people 
 # with tattoos who regret getting one. 
 
+# t.test is for numerical variables
+# tests or confidence intervals for variables such as
+# math scores, reading scores, ages, ..
+
+# prop.test is for proportions
+# proportion of the population that regrets getting a tattooo
+# the proportion of population that votes for a particular
+# political party, so on..
+
+# estimating: prop of people regretting
+# getting a tattoo
+
+# x = "number of people regretting the tattoo"
+# n = "sample size of the study"
+
 # use prop.test, conf.level = 0.99
+prop.test(x = 59, n = 423, conf.level = 0.99)
+# 99 percent confidence interval:
+#   [0.1006219, 0.1897715]
+# Confidence interval for the proportion of people
+# who regret getting a tattoo in the general population
+
+# 59 regretted getting the tattoo
+# 423 interviewed
+59/423 # sample proportion
+
+# interval estimate that quantifies my uncertainty
+# in the estimation in some sense
+# 99 percent confidence interval:
+# [0.1006219, 0.1897715] 
+# interval estimate for the population proportion of 
+# people who regret getting the tattoo
+
+# 99% confidence intervals contain the unknown
+# population parameter 99% of time 
+
+# (I'm making it up)
+# null: proportion of people who regret
+# getting a tattoo is 0.2 (20%)
+# alternative: proportion of people 
+# who regret getting a tattoo is not
+# 0.2 
+prop.test(x = 59, n = 423, p = 0.2, conf.level = 0.99)
+# alternative hypothesis: true proportion is not equal to 0.2
+# p.value = 0.002281
+prop.test(x = 59, n = 423) # required
+# default options: p = 0.5, conf.level = 0.95
+
+# p refers to the value of the true proportion 
+# under the null hypothesis
+# null: true population proportion is equal to p
+# alternative: true population proportion is not equal to p
+
+# conf.level refers to the confidence of the confidence
+# interval that is given in the output
+# confidence interval is an interval estimate of 
+# the true proportion
+# the higher the confidence level, the wider the interval
 
 
-# test p = 0.2 against p less than 0.2
+
+
+# numerical variables:
+# null: population mean is equal to 3
+# t.test(hsb2$math, mu = 3, conf.level = ...)
+
+# proportions:
+# null: population proportion is equal to 0.2
+# x = number of people who regret getting a tattoo
+# n = sample size of the study
+# conf.level = 
+# prop.test(x = , n = , p = 0.2, conf.level = ...)
 
 
 ## Two proportions
 
+# OK cupid
 
 #  In a study about online dating, 
 # 9 out of 40 males lied about their age
-# and 5 out of 40 females lied about their age. Find a 95% confidence
-# interval for the difference
-# (% of men who lie about their age) - (% of women who lie about their age).
+# and 5 out of 40 females lied about their age. 
 
-# use prop.test
+# can we test whether these sample differences
+# are statistically significant or not?
+
+# proportion / percentage:
+# how many people in a sample satisfy a certain thing 
 
 
+# this is going to be a number which is going to be 
+# between 0 (nobody satisfies it) and the sample (40 people)
+
+# how many people in my sample regret getting a tattoo
+# proportion: 9/40 (men) to 5/40 (women)'
+
+# numerical (average math score)
+# numbers that are relative to variable and they
+# are not constrained by, for example, the sample size
+
+
+t.test(x = c(9, 5), n = c(40, 40))
+# ignore n
+# it gets x
+# and it tests
+# null hypothesis: mean of x is equal to 0
+# alternative hypothesis: mean of x not equal to 0
+
+
+# answer: yes, we'll use the prop.test
+prop.test(x = c(9, 5), n = c(40, 40))
+# null: population proportions are the same
+# alternative: population proportions differ
+# p.value = 0.3774 > 0.01 => do not reject the null
+
+# this gives us a confidence interval as well
+# proportion ("first group" - "second group")
+# data for men first
+# difference in proportions "male - female"
+# the interval contains 0 so the difference among
+# male and female is not significant
+
+# Does not contain zero
+# Differences are positive 
+# proportion first group > proportion second group
+# [0.1, 0.3]
+
+# [0.0000001, 0.0000003]
+# some people like reporting CI for differences
+# better than p-values because they contain information
+# about magnitude and sign of effects which p-values
+# do not contain
 
 # test H0: men and women lie at the same rate
 # against H1: men lie more than women
 
 
-## chi-squared tests of independence for categorical variables
-
-# read in hsb2
-library(openintro)
-data(hsb2)
-
-# a team of social scientists want to test 
-# whether the distribution of `ses` depends on `race`
-
-# visualize the data
-
-# do a chi-squared test 
-
-
-
-
-#############################
-# Exploratory data analysis #
-#############################
-
-# read in http://vicpena.github.io/sta9750/spring19/nyc.csv
-install.packages("GGally")
-library(GGally)
-
-# Create a figure that contains plots for all the pairs of variables 
-#   in the dataset, except Case and Restaurant
-
-# Provide a heatmap for the correlation 
-#  between the numerical variables in the dataset. 
-
-
-# Find examples of cheap restaurants that 
-# have relatively good food and examples of 
-# expensive restaurants that have relatively bad food 
+# prop.test(x, n, p )
+# Required inputs:
+# x = number of successes / events
+# n = sample size 
+# Optional: 
+# p = value of the true proportion that you hypothesize
+# under the null hypothesis
+# conf.level = confidence level of the test
