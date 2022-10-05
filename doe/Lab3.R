66d9e98ea1d37890975a6eebeb15a3d4f3ad1dab
Víctor Peña <VicPena@users.noreply.github.com>
2022-10-05 14:57:54 +0200

    Add files via upload

---
+++ doe/Lab3.R
@@ -0,0 +1,51 @@
+# DoE Lab 3: script for reading in data
+
+##1.1.##
+  
+  
+gasos = read.csv2("http://vicpena.github.io/doe/lab3/Gases.csv")
+
+
+
+##1.2.##
+  
+  
+rates = read.csv2("http://vicpena.github.io/doe/lab3/Rates.csv")
+
+
+
+##1.3.##
+  
+  
+pa = read.csv2("http://vicpena.github.io/doe/lab3/Pan.csv")
+
+
+##1.4.##
+  
+  
+aigua = read.csv2("http://vicpena.github.io/doe/lab3/Aigua.csv")
+
+
+##2.1.## 
+  
+  
+farmac = read.csv2("http://vicpena.github.io/doe/lab3/Farmaco.csv")
+
+
+##2.2.## 
+  
+  
+cotxes = read.csv2("http://vicpena.github.io/doe/lab3/Polucion.csv")
+
+
+##2.3.##
+  
+  
+sulfur = read.csv2("http://vicpena.github.io/doe/lab3/Sulfuro.csv") 
+
+
+
+##2.4.##
+  
+  
+mosques = read.csv2("http://vicpena.github.io/doe/lab3/Mosques.csv") 
